b0851cf422afcdfa3b845c77e48cb2bdbb235b53
VKaramanliev <vasko.karamanliev@gmail.com>
2026-05-26 17:37:44 +0300

    PPMImage class realization

--- src/PPMImage.cpp
+++ src/PPMImage.cpp
@@ -1,3 +1,157 @@
 #include "../include/PPMImage.hpp"
 
-PPMImage::PPMImage(const string& name) : Image(name) {}
+#include <iostream>
+#include <fstream>
+
+PPMImage::PPMImage(const string& name) : Image(name), maxValue(255) {}
+
+void PPMImage::load() {
+    ifstream file(name);
+
+    if (!file.is_open()) {
+        cout << "Cannot open file: " << name << endl;
+        return;
+    }
+
+    string format;
+    file >> format;
+
+    if (format != "P3") {
+        cout << "Invalid file format!" << endl;
+        file.close();
+        return;
+    }
+
+    file >> width >> height >> maxValue;
+    pixels.clear();
+    pixels.resize(height, vector<RGB>(width));
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            file >> pixels[i][j].r >> pixels[i][j].g >> pixels[i][j].b;
+        }
+    }
+
+    file.close();
+}
+
+void PPMImage::save() const {
+    ofstream file(name);
+
+    if (!file.is_open()) {
+        cout << "Cannot save file: " << name << endl;
+        return;
+    }
+
+    file << "P3" << endl;
+    file << width << " " << height << endl;
+    file << maxValue << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            file << pixels[i][j].r << " "
+                 << pixels[i][j].g << " "
+                 << pixels[i][j].b << " ";
+        }
+
+        file << endl;
+    }
+
+    file.close();
+}
+
+void PPMImage::saveas(const string& newName) const {
+    ofstream file(newName);
+
+    if (!file.is_open()) {
+        cout << "Cannot save file: " << name << endl;
+        return;
+    }
+
+    file << "P3" << endl;
+    file << width << height << endl;
+    file << maxValue << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            file << pixels[i][j].r << " "
+                 << pixels[i][j].g << " "
+                 << pixels[i][j].b << " ";
+        }
+
+        file << endl;
+    }
+
+    file.close();
+}
+
+void PPMImage::grayscale() {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int gray = (pixels[i][j].r + pixels[i][j].g + pixels[i][j].b) / 3;
+
+            pixels[i][j].r = gray;
+            pixels[i][j].g = gray;
+            pixels[i][j].b = gray;
+        }
+    }
+}
+
+void PPMImage::monochrome() {
+    grayscale();
+
+    int middle = maxValue / 2;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int value;
+
+            if (pixels[i][j].r > middle) {
+                value = maxValue;
+            } else {
+                value = 0;
+            }
+
+            pixels[i][j].r = value;
+            pixels[i][j].g = value;
+            pixels[i][j].b = value;
+        }
+    }
+}
+
+void PPMImage::negative() {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            pixels[i][j].r = maxValue - pixels[i][j].r;
+            pixels[i][j].g = maxValue - pixels[i][j].g;
+            pixels[i][j].b = maxValue - pixels[i][j].b;
+        }
+    }
+}
+
+void PPMImage::rotate(const string& direction) {
+    vector<vector<RGB>> rotated;
+
+    if (direction == "right") {
+        rotated.resize(width, vector<RGB>(height));
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                rotated[j][height - 1 - i] = pixels[i][j];
+            }
+        }
+    } else if (direction == "left") {
+        rotated.resize(width, vector<RGB>(height));
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                rotated[width - 1 - j][i] = pixels[i][j];
+            }
+        }
+    }
+
+    pixels = rotated;
+    
+    int oldWidth = width;
+    width = height;
+    height = width;
+}
